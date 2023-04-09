6b67d299ff799f21c4d33c1b09ed3569e768cf88
Jan Streffing <jan.streffing@awi.de>
2023-04-09 17:29:44 +0200

    add conserv plot

---
+++ combine_conserv.sh
@@ -0,0 +1,5 @@
+montage -geometry +40+40\
+   conserv_north_sea.png \
+   conserv_atlantic.png \
+  -background white -fill white -bordercolor white -border 0 \
+  conserv_remapping.png
---
+++ combine_gauswgt.sh
@@ -0,0 +1,7 @@
+montage -geometry +40+40\
+   gauswgt_north_sea_N25.png \
+   gauswgt_north_sea_N4.png \
+   gauswgt_atlantic_N25.png \
+   gauswgt_atlantic_N4.png \
+  -background white -fill white -bordercolor white -border 0 \
+  gaussian_remapping.png
